32c28c3fe791d813b47206745b52728229d27992
chengJiaWei <1403642083@qq.com>
2023-10-24 20:59:57 +0800

    create TwoDimensionalArrayTest.java

---
+++ JavaSECode/src/main/java/org/example/OOBasics.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @ClassName: OOBasics
+ * @Package: org.example
+ * @Author cheng
+ * @Create 2023/10/23 19:12
+ * @Description: 面向对象基础部分代码
+ */
+public class OOBasics {
+    public static int age;
+}
