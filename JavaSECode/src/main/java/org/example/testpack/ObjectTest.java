5bbde270c93506314dd6424fa65bf7ce582c367e
cheng <1403642083@qq.com>
2023-11-09 15:38:59 +0800

    super关键字的使用

---
+++ JavaSECode/src/main/java/org/example/testpack/ObjectTest.java
@@ -0,0 +1,18 @@
+package org.example.testpack;
+
+import org.junit.Test;
+
+/**
+ * @Author cheng
+ * @ClassName ObjectTest
+ * @Date 2023/11/9 14:35
+ * @Version V1.0
+ * @Description
+ */
+public class ObjectTest {
+    @Test
+    public void test(){
+        Person p1 = new Person();
+
+    }
+}
